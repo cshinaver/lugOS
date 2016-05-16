14960298215029a52592b8668288550850c4886e
cshinaver <bshinaver@gmail.com>
2016-05-16 01:48:47 -0400

    Fixed tokenqueue insert test

--- tests/test_queue.cc
+++ tests/test_queue.cc
@@ -1,12 +1,13 @@
 #include <gtest/gtest.h>
 
 #include "token.h"
+#include "queue.h"
 
 TEST(TokenQueueTestCase, TokenQueueInsertTest) {
     Token t1, t2;
-    TokenQueue q;
-    t.type = STRING_LITERAL;
-    q.insert(t);
-    t2 = q.pop();
+    TokenQueue q = token_queue_create();
+    t1.type = STRING_LITERAL;
+    token_queue_insert(q, t1);
+    t2 = token_queue_pop(q);
     ASSERT_EQ(t1.type, t2.type);
 }
